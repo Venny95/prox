b742c90791f56871caaffd644da6960bc9a0ff13
Venla Heiskanen <venla.heiskanen@karelia.fi>
2025-12-05 13:23:38 +0200

    Tietokantayhteys saatu toimimaan ja tietokannasta saatu luettua aineistot R:ään.R

--- prox_code.R
+++ prox_code.R
@@ -1,3 +1,28 @@
 library(dodgr)
 
-#kokeilu
+library(DBI)
+library(RPostgres)
+library(sf)
+
+#file.edit("~/.Renviron")
+#dbDisconnect(con)
+
+#Avataan tietokantayhteys (.Renviron)
+con <- dbConnect(
+  RPostgres::Postgres(),
+  host     = Sys.getenv("PG_HOST"),
+  port     = as.integer(Sys.getenv("PG_PORT")),
+  dbname   = Sys.getenv("PG_DB"),
+  user     = Sys.getenv("PG_USER"),
+  password = Sys.getenv("PG_PASSWORD"),
+  sslmode  = Sys.getenv("PG_SSLMODE") # esim. "require" tuotannossa
+)
+
+# Tarkistetaan yhteys
+dbIsValid(con)
+dbGetQuery(con, "SELECT current_database(), current_user, current_timestamp;")
+dbListTables(con)
+
+#Luetaan aineistot tietokannasta
+roads <- dbReadTable(con, "digiroad_tielinkin_tyyppi_joensuu")
+buil <- dbReadTable(con, "joensuu_buildings_20251029")
